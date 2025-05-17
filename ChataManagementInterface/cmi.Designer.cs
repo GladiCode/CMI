aa841a294eff1207677ffc6bcc719f0ab4ea963a
Petrof <lklukashonek210@gmail.com>
2025-05-17 13:03:37 +0200

    Add files via upload

---
+++ ChataManagementInterface/cmi.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.chataTVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nazivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bazenCamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.babinaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stavToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importovatCisloAkcieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnPrihlasit = new System.Windows.Forms.Button();
+            this.btnTerminal = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.babinaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.chataTVToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(50, 20);
+            this.toolStripMenuItem1.Text = "Chata";
+            // 
+            // chataTVToolStripMenuItem
+            // 
+            this.chataTVToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nazivoToolStripMenuItem});
+            this.chataTVToolStripMenuItem.Name = "chataTVToolStripMenuItem";
+            this.chataTVToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.chataTVToolStripMenuItem.Text = "Chata TV";
+            // 
+            // nazivoToolStripMenuItem
+            // 
+            this.nazivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bazenCamToolStripMenuItem});
+            this.nazivoToolStripMenuItem.Name = "nazivoToolStripMenuItem";
+            this.nazivoToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.nazivoToolStripMenuItem.Text = "Nazivo";
+            // 
+            // bazenCamToolStripMenuItem
+            // 
+            this.bazenCamToolStripMenuItem.Name = "bazenCamToolStripMenuItem";
+            this.bazenCamToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.bazenCamToolStripMenuItem.Text = "Bazen Cam";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(57, 20);
+            this.toolStripMenuItem2.Text = "Pivnica";
+            // 
+            // babinaToolStripMenuItem
+            // 
+            this.babinaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stavToolStripMenuItem,
+            this.logToolStripMenuItem,
+            this.importovatCisloAkcieToolStripMenuItem});
+            this.babinaToolStripMenuItem.Name = "babinaToolStripMenuItem";
+            this.babinaToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.babinaToolStripMenuItem.Text = "Babina";
+            // 
+            // stavToolStripMenuItem
+            // 
+            this.stavToolStripMenuItem.Name = "stavToolStripMenuItem";
+            this.stavToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.stavToolStripMenuItem.Text = "Stav";
+            // 
+            // logToolStripMenuItem
+            // 
+            this.logToolStripMenuItem.Name = "logToolStripMenuItem";
+            this.logToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.logToolStripMenuItem.Text = "Log";
+            // 
+            // importovatCisloAkcieToolStripMenuItem
+            // 
+            this.importovatCisloAkcieToolStripMenuItem.Name = "importovatCisloAkcieToolStripMenuItem";
+            this.importovatCisloAkcieToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.importovatCisloAkcieToolStripMenuItem.Text = "Importovat Cislo Akcie";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(688, 415);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // btnPrihlasit
+            // 
+            this.btnPrihlasit.Location = new System.Drawing.Point(713, 27);
+            this.btnPrihlasit.Name = "btnPrihlasit";
+            this.btnPrihlasit.Size = new System.Drawing.Size(75, 23);
+            this.btnPrihlasit.TabIndex = 6;
+            this.btnPrihlasit.Text = "Prihlasit";
+            this.btnPrihlasit.UseVisualStyleBackColor = true;
+            // 
+            // btnTerminal
+            // 
+            this.btnTerminal.Location = new System.Drawing.Point(713, 56);
+            this.btnTerminal.Name = "btnTerminal";
+            this.btnTerminal.Size = new System.Drawing.Size(75, 23);
+            this.btnTerminal.TabIndex = 7;
+            this.btnTerminal.Text = "Terminal";
+            this.btnTerminal.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(713, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Kontaktovat";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(713, 114);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Akcie";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnTerminal);
+            this.Controls.Add(this.btnPrihlasit);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Chata Management Interface";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem chataTVToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nazivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bazenCamToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem babinaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stavToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importovatCisloAkcieToolStripMenuItem;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnPrihlasit;
+        private System.Windows.Forms.Button btnTerminal;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ChataManagementInterface/cmi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
